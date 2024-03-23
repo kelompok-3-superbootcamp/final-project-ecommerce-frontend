8a991ad2b58ff2debf0057e1d054e2f477570cfb
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2024-03-23 16:10:04 +0700

    feat(type): crud type dashboard

--- pages/dashboard/type/index.jsx
+++ pages/dashboard/type/index.jsx
@@ -1,5 +1,12 @@
-import axios from "axios"
 import { host } from "@/utils/constant"
+import { Button } from "antd"
+import axios from "axios"
+import { Table } from "flowbite-react"
+import { useEffect, useState } from "react"
+import { FaEdit, FaTrash } from "react-icons/fa"
+import Swal from "sweetalert2"
+import Link from "next/link"
+import { useAuthStore } from "../../../stores/auth"
 
 export async function getServerSideProps() {
   const props = {
@@ -18,73 +25,109 @@ export async function getServerSideProps() {
     props.perdata = null
   }
 
-  console.log(props)
+  console.log(props, host)
 
   return { props }
 }
 
-const Type = () => {
+const Type = ({ perdata }) => {
+  const [types, setTypes] = useState({
+    refetch: false,
+    data: perdata,
+  })
+
+  const user = useAuthStore(state => state.user)
+
+  const handleDelete = async id => {
+    const sure = confirm("Yakin mau hapus?")
+
+    if (sure) {
+      try {
+        const response = await axios.delete(`${host}/types/${id}`, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+          },
+        })
+
+        Swal.fire({
+          icon: "success",
+          title: "Success",
+          text: "Berhasil hapus type",
+        })
+        console.log(response)
+        setTypes({
+          ...types,
+          refetch: true,
+        })
+      } catch (err) {
+        Swal.fire({
+          icon: "error",
+          title: "Failed",
+          text: err?.response.data.message ?? "Hapus Gagal",
+        })
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (types.refetch) {
+      axios
+        .get(`${host}/types`)
+        .then(res => {
+          setTypes({
+            refetch: false,
+            data: res.data.data,
+          })
+
+          console.log(res)
+        })
+        .catch(err => console.log(err))
+    }
+  }, [types])
+
   return (
     <>
       <div className="float-end w-auto">
-        <Button type="primary" size={"large"}>
-          Tambah Tipe Mobil
-        </Button>
+        <Link href="/dashboard/type/create">
+          <Button type="primary" size={"large"}>
+            Tambah Tipe Mobil
+          </Button>
+        </Link>
       </div>
       <div className="mt-20 overflow-x-auto">
         <Table striped>
           <Table.Head>
             <Table.HeadCell>No</Table.HeadCell>
-            <Table.HeadCell>Nama Model</Table.HeadCell>
+            <Table.HeadCell>Nama Tipe</Table.HeadCell>
             <Table.HeadCell>Deskripsi</Table.HeadCell>
-            <Table.HeadCell>Harga</Table.HeadCell>
-            <Table.HeadCell>Transmisi</Table.HeadCell>
-            <Table.HeadCell>Kondisi</Table.HeadCell>
-            <Table.HeadCell>Tahun Produksi</Table.HeadCell>
-            <Table.HeadCell>Kilometer</Table.HeadCell>
-            <Table.HeadCell>Stok</Table.HeadCell>
-            <Table.HeadCell>Warna</Table.HeadCell>
-            <Table.HeadCell>Lokasi</Table.HeadCell>
-            <Table.HeadCell>Gambar</Table.HeadCell>
-            <Table.HeadCell>Brand</Table.HeadCell>
-            <Table.HeadCell>Tipe</Table.HeadCell>
-            <Table.HeadCell>Seller</Table.HeadCell>
             <Table.HeadCell>Action</Table.HeadCell>
           </Table.Head>
           <Table.Body className="divide-y">
-            {data?.map((item, index) => (
-              <>
-                <Table.Row key={item.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <Table.Cell>{index + 1}</Table.Cell>
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    {item?.name}
-                  </Table.Cell>
-                  <Table.Cell>{item?.description}</Table.Cell>
-                  <Table.Cell>{item?.price}</Table.Cell>
-                  <Table.Cell>{item?.transmission}</Table.Cell>
-                  <Table.Cell>{item?.condition}</Table.Cell>
-                  <Table.Cell>{item?.year}</Table.Cell>
-                  <Table.Cell>{item?.km}</Table.Cell>
-                  <Table.Cell>{item?.stock}</Table.Cell>
-                  <Table.Cell>{item?.color}</Table.Cell>
-                  <Table.Cell>{item?.location}</Table.Cell>
-                  <Table.Cell>{item?.image}</Table.Cell>
-                  <Table.Cell>{item?.brand_name}</Table.Cell>
-                  <Table.Cell>{item?.type_name}</Table.Cell>
-                  <Table.Cell>{item?.user_name}</Table.Cell>
-                  <Table.Cell>
-                    <div className="flex items-center gap-2">
-                      <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                        <FaEdit className="text-yellow-300" />
-                      </a>{" "}
-                      |
-                      <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                        <FaTrash className="text-red-500" />
-                      </a>
-                    </div>
-                  </Table.Cell>
-                </Table.Row>
-              </>
+            {types.data?.map((item, index) => (
+              <Table.Row key={item.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell>{index + 1}</Table.Cell>
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {item?.name}
+                </Table.Cell>
+                <Table.Cell>{item?.description}</Table.Cell>
+                <Table.Cell>
+                  <div className="flex items-center gap-2">
+                    <Link
+                      href={`/dashboard/type/edit/${item.id}`}
+                      className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                    >
+                      <FaEdit className="text-yellow-300" />
+                    </Link>{" "}
+                    |
+                    <button
+                      onClick={() => handleDelete(item.id)}
+                      className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                    >
+                      <FaTrash className="text-red-500" />
+                    </button>
+                  </div>
+                </Table.Cell>
+              </Table.Row>
             ))}
           </Table.Body>
         </Table>

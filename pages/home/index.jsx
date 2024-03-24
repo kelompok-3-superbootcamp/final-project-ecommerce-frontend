296e480e145a3dcd0a4e78f5eefb377864a540f2
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2024-03-24 12:00:14 +0700

    feat(checkout): apply voucher

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,19 +1,19 @@
-'use client';
+"use client"
 
-import Slider from '@/components/slider';
-import Grid from '@/components/Grid';
-import {useAuthStore} from "@/stores/auth"
+import Slider from "@/components/slider"
+import Grid from "@/components/grid"
+import { useAuthStore } from "@/stores/auth"
 
 const Home = () => {
-    const {user, logout} = useAuthStore()
-    console.log(user)
-    return (
-        <>
-            {/* {user} */}
-            <Slider />
-            <Grid />
-        </>
-    );
-    }
+  const { user, logout } = useAuthStore()
+  console.log(user)
+  return (
+    <>
+      {/* {user} */}
+      <Slider />
+      <Grid />
+    </>
+  )
+}
 
 export default Home
